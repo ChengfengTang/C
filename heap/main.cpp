a062569cb24bba8caba4c705be27e33a1252e880
ChengfengTang <42978810+ChengfengTang@users.noreply.github.com>
2019-03-11 17:56:38 -0700

    Update main.cpp

--- heap/main.cpp
+++ heap/main.cpp
@@ -7,69 +7,124 @@
 #include <iomanip>
 using namespace std;
 
-
+void printArray (int intarray[],int rows, int longestnumber, int numbers);
 char* input;
 char* numbers;
-void printArray(int array[])
+void printArray (int intarray[],int rows, int longestnumber, int numbers)
 {
-       
-  cout << endl;
-  cout << endl;
-  int f = 0;
  
-  for (f = 0; f <= 100; f++)
-    {
-      if (array[f] == -1)
-	{
-	  break;
-	}
-    
-    }
-  int rows = log2(f) + 1 ;
- 
+  int indexbegin = pow(2,rows-1) -1;  // if the row that's passed in is 3, index starts 3 which is the 4th element
+  int indexend = pow(2,rows) -1; // and ends at 7, which is the 8th element
   
-  //cout << rows << endl;
+   
+  int arrayindex = 0; // keep track of the elements in the elementsindex
+  int numbersofelements = pow(2,rows-1);
+  cout << "Numbers of number in the bottom row: " << numbersofelements << endl;
+  int elementsindex [numbersofelements] = {};
 
- 
-  cout << endl;
-  cout <<setw((rows)*3.5) << " " <<  array[0] << endl;
-     
-  int min = 1;
-  int max = 1;
-  int currentrow = log2(min) + 1;
-  while (max <= 100)
+  for (int i = 1; i<= numbersofelements; i++ )
     {
-      min = 2 * min;
-      max = 2 * max + 1;
-      currentrow = log2(min) + 1 ;
-      //cout << "currentrow" << currentrow << endl;
       
-      for (int i = min; i <= max; i++)
+      int position = ((i-1) * longestnumber) + (i-1)* 1 ; // 1 here is spacing between each element
+      elementsindex[arrayindex] = position;
+      
+      arrayindex++;
+      
+    }
+  
+  //calculate everything above this level now
+  int d = 0;
+  int m = numbersofelements -1;
+  
+  for(int r = rows; r > 1; r--)
+    {
+       
+      for ( int v = d;  v<= m ; v++)
 	{
-	  
-	  if (array[i-1] == -1)
+	  if (v%2 ==0 )
 	    {
-	      break;
-	      break;
-	    }
-	  else
-	    {
-	      if ( i/2 != (i+1)/2)
-		{
-		  
-		  cout << setw( pow(2 ,(rows-currentrow)) *3 ) << " " << array[i-1] ;
-		}
-	      else
-		{
-		  cout << setw( (rows-currentrow+1) *3 -1)  << " " << array[i-1] ;
-		}
-		    
-	       
+	      
+	      int add =  (elementsindex[v] + elementsindex[v+1]) /2;
+	      
+	     
+	      elementsindex[arrayindex] = add;
+	      
+	      arrayindex++;
+	      
 	    }
 	}
-      cout << endl;
+       d = m +1;
+      
+      m += pow(2,r-2);
     }
+  elementsindex[arrayindex] == '\0';
+  cout << arrayindex << endl;
+  for (int n = 0           ; n<=arrayindex-1  ; n++ )
+    {
+      cout << "Index: " <<elementsindex[n] << endl;
+      }
+
+  
+  int length = longestnumber*pow(2,rows-1) + (pow(2,rows-1) * 1 -1) - 1;
+  cout << "Every row should have a index length of: " << length << endl;
+int a = 0;
+
+ for (int j = rows; j > 0; j--)
+  {
+    int x = pow(2,j-1) -1;
+    if (x == 0) // log 0 is undefined, since the top tree is always the biggest number therefore, just print it out.
+      {
+	
+	for (int lastforloopipromise = 0; lastforloopipromise < elementsindex[a]; lastforloopipromise++)
+	  {
+	    cout <<" ";
+	  }
+	int l = log10(intarray[0])+1;
+	if (l % 2 == 0)
+	  {
+	    cout << "0" ; // add a 0 if it has even nums of digits
+	  }
+	cout << intarray[0] << endl;
+      }
+    for (int i = 0; i<= length;)
+      {
+         
+	if (i == elementsindex[a])
+	  {
+	  
+	    if ((intarray[x] != -1) && (x !=0))
+	      {
+		int p = log10(intarray[x])+1;
+		if (p <= longestnumber)
+		  {
+		    for (int b = 0; b< longestnumber-p; b++)
+		      {
+			cout << "0"; // 1 -> 001 if the longest digit is 3
+		      }
+		    cout << intarray[x];
+		    i += longestnumber;
+		  
+		    a++;
+		    x++;
+		  }
+	      }
+	    else
+	      {
+		break;
+	      }
+	 
+	  }
+	else
+	  {
+	    cout <<" ";
+	    i++;
+	  }
+      }
+    cout << endl;
+  
+  }
 }
+
 void checkbackward(int array[], int i)
 { // everytime a number is replaced with an element after it check everything before the number and make sure they are all bigger than the element 
 
@@ -136,8 +191,48 @@ void processArray(int array[])
       
       i ++ ;
     }
+  int longestnumber = 0;
+  for (int i = 0; i<= 100; i++)
+    {
+      if (array[i] != -1)
+	{
+	  if (log10(array[i]) +1 > longestnumber)
+	    {
+	      longestnumber = log10(array[i])+1; 
+	    }
+	}
+      else
+	{
+	  break;
+	}
+    }
   
-  printArray(array);
+  if (longestnumber %2 == 0) // make sure it's an odd digit.
+    {
+      longestnumber ++;
+    }
+  cout << "The longestnumber in the array list has a digit of: " << longestnumber << endl; 
+  int numbers = 0;
+  for ( int i = 0; i <= sizeof(intarray); i++)
+    {
+      if (array[i] != -1)
+        {
+	  cout << array[i] << " ";
+        }
+      else
+        {
+	  numbers = i;
+	  break;
+        }
+        
+    } // print out all the valid numbers, which are non -1s
+  cout << endl;
+
+  //calculate # of rows
+  int rows =  log2(numbers) + 1;
+    
+  printArray (array, rows,longestnumber, numbers);
+ 
   
 }
 
