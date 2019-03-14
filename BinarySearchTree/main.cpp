fe3ec162ec3727c8def59e4648256f1790fe928a
ChengfengTang <465783@bsd48.org>
2019-03-13 21:28:20 -0700

    Need to delete and print

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -8,26 +8,34 @@
 #include <fstream>
 #include <math.h>
 using namespace std;
-void add(int array[], Node* &head);
+void add(int array[], Node* &head, int size);
 char* input;
 char* numbers;
 int main()
 {
   cout << "Welcome to Chengfeng Tang's Binary Search Project" << endl ;
-
-  int a = 0;
+  cout << "To start you would have to enter some numbers!" << endl;
+  int a = 10;
+  
   int array[100];
+  
   Node* head = NULL;
+  
   for (int abc = 0; abc <= 100; abc++)
     {
       array[abc] = -1;
     }
-
-  while( a!= 3)
+  
+  
+  while( a > 3 || a < 1)
     {
+      
+      cout << "--------------------------------------------" << endl;
       cout << "Enter 1 if you would like to enter the numbers manually." << endl;
       cout << "Enter 2 if you would like to read in a file with inputs." << endl;
       cout << "Enter 3 if you would like to close the program." << endl;
+      
+      cout << "--------------------------------------------" << endl;
       cin >> a;
       cin.get();
 
@@ -35,7 +43,11 @@ int main()
 
       if (a == 1)
 	{
+	  
+      cout << "--------------------------------------------" << endl;
 	  cout << "Please enter numbers with spaces between them (put a space after the last number)." << endl;
+	  
+       cout << "--------------------------------------------" << endl;
 	  cin.get(input,100);
 	  cin.get();
 
@@ -81,18 +93,27 @@ int main()
 	      d = atoi(numbers); // turn char array into a number
 	      array[arrayindex++] = d; // add to the int array
 	    }
+	  
+      cout << "--------------------------------------------" << endl;
 	  cout << " The numbers are: " << endl;
 	  for (int i = 0; i <= arrayindex-1; i++) // print the array  out
 	    {
 	      cout << array[i] << " ";
 	    }
+	  int size = arrayindex;
 	  cout << endl;
-	  add(array, head);
-	  return 0;
+	  
+      cout << "--------------------------------------------" << endl;
+	  add(array, head, size);
+	 
 	}
       else if (a ==2)
 	{
+	  
+      cout << "--------------------------------------------" << endl;
 	  cout << "please enter the name of the test file." << endl;
+	  
+      cout << "--------------------------------------------" << endl;
 	  cin.get(input,100);
 	  cin.get();
 	  ifstream myfile;
@@ -127,15 +148,15 @@ int main()
 	      
 		}
 	      cout << endl;
-	      add (array,head);
+	      add (array,head, size);
 	      
-	      return 0;
+	      
 	    }
 	}
       else if (a == 3)
 	{
 	  cout << "Bye" << endl;
-	  
+	  return 0;
 	}
       else
 	{
@@ -144,14 +165,126 @@ int main()
 	}
 
     }
+  cout << endl;
 
-  return 0;
+  // now we have a basic tree, we can start doing stuff with it.
+  int b = 0;
+  while (b != 4)
+    {
+      cout << "--------------------------------------------" << endl;
+      cout << "Now if you would like to add a Node, enter 1" << endl;
+      cout << "If you would like to delete a Node, enter 2" << endl;
+      cout << "If you would like to print the tree, enter 3" << endl;
+      cout << "If you would like to exit, enter 4" << endl;
+      
+      cout << "--------------------------------------------" << endl;
+      cin >> b;
+      cin.get();
+      if (b == 1)
+	{
+	  cout << endl;
+	  cout << "What's the number that you want to add?" << endl;
+	  
+      cout << "--------------------------------------------" << endl;
+	  
+	  int temp = 0;
+	  cin >> temp;
+	  cin.get();
+	  int size2 = 0;
+	  while (array[size2] != -1)
+	    {
+	      size2++;
+	    }
+	  array[size2] = temp;
+	  size2++;
+	  add (array, head, size2);
+	  //Singleadd
+	}
+      else if (b==2)
+	{
+	  cout << endl;
+	  cout << "What's the number that you want to delete?"<<endl;
+	  cout << endl;
+	  //delete
+	}
+      else if (b==3)
+	{
+	  cout << "-----------------------------------------" << endl;
+	}
+      else if (b==4)
+	{
+	  cout << "bye" << endl;
+	  return 0;
+	}
+      else
+	{
+	  cout << "Invalid #" << endl;
+	  cout << endl;
+	}
+
+
+
+
+
+    }
+  
 }
-void add (int array[], Node* &head)
+void add (int array[], Node* &head, int size)
 {
   
-  if (head == NULL)
+      cout << "--------------------------------------------" << endl;
+  cout << "There are " << size << " nodes" << endl;
+  
+      cout << "--------------------------------------------" << endl;
+  for (int i = 0; i <= size; i++)
     {
-      array[i] ==
+      if (head == NULL)
+	{
+	  Node* head = new Node();
+	
+	  head->setValue(array[i]);
+	}
+      else
+	{
+	  Node * current = head;
+	  while (head != NULL)
+	    {
+	      if (array[i] < current->getValue())
+		{
+		  if (current->getLeft() == NULL)
+		    {
+		      Node* node = new Node();
+		      node->setValue(array[i]);
+				    current->setRight(node);
+				    break;
+				    }
+		      else
+			{
+			  cout << "smaller" << endl;
+			  current->getLeft();
+
+			}
+		    }
+		  else if (array[i] > current->getValue())
+		    {
+		      if (current->getRight() == NULL)
+			{
+			  Node* node = new Node();
+			  node->setValue(array[i]);
+			  current->setRight(node);
+			  break;
+			}
+		      else
+			{
+			  cout << "bigger" << endl;
+			  current->getRight();
+			}
+		    }
+		}
+	    
+	    
+	    }
+	}
+  
     }
-}
+
