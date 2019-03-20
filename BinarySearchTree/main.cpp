4ff07bbf82eb78e8cc360af275047aa1af36b20f
ChengfengTang <42978810+ChengfengTang@users.noreply.github.com>
2019-03-20 16:58:20 -0700

    Update main.cpp

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -9,6 +9,7 @@
 #include <math.h>
 using namespace std;
 
+void printArray (int intarray[],int rows, int longestnumber);
 void deleteNode(Node* & n, Node* &parent, Node* &head);
 void findNode(Node*  &head, int number, Node* &parent);
 void buildTree(int array[], Node* &head, int size);
@@ -612,4 +613,153 @@ void buildTree (int array[], Node* &head, int size)
     }
   
 }
+void printArray (int intarray[],int rows, int longestnumber)
+{
+ 
+  int indexbegin = pow(2,rows-1) -1;  // if the row that's passed in is 3, index starts 3 which is the 4th element
+  int indexend = pow(2,rows) -1; // and ends at 7, which is the 8th element
+
+  for (int i= indexbegin; i<= indexend; i++)
+    {
+      if (intarray[i] == -1)
+	{
+	  intarray[i] = -2;
+	}
+    }
+  /*for (int i =0; i<= 100; i++)
+    {
+      cout << intarray[i] << endl;
+      if (intarray[i] == -1)
+	{
+	  break;
+	}
+    }*/
+   
+  int arrayindex = 0; // keep track of the elements in the elementsindex
+  int numbersofelements = pow(2,rows-1);
+  //cout << "Numbers of number in the bottom row: " << numbersofelements << endl;
+  int elementsindex [numbersofelements] = {};
+
+  for (int i = 1; i<= numbersofelements; i++ )
+    {
+      
+      int position = ((i-1) * longestnumber) + (i-1)* 1 ; // 1 here is spacing between each element
+      elementsindex[arrayindex] = position;
+      
+      arrayindex++;
+      
+    }
+  
+  //calculate everything above this level now
+  int d = 0;
+  int m = numbersofelements -1;
+  
+  for(int r = rows; r > 1; r--)
+    {
+       
+      for ( int v = d;  v<= m ; v++)
+	{
+	  if (v%2 ==0 )
+	    {
+	      
+	      int add =  (elementsindex[v] + elementsindex[v+1]) /2;
+	      
+	     
+	      elementsindex[arrayindex] = add;
+	      
+	      arrayindex++;
+	      
+	    }
+	}
+       d = m +1;
+      
+      m += pow(2,r-2);
+    }
+  elementsindex[arrayindex] == '\0';
+  //cout << arrayindex << endl;
+  int z = 0 ;
+  for (int n = 0           ; n<=arrayindex-1  ; n++ )
+    {
+      cout << "Index: " <<elementsindex[n] << endl;
+      z = elementsindex[n];
+    }
+
+  
+  int length = longestnumber*pow(2,rows-1) + (pow(2,rows-1) * 1 -1) - 1;
+  //cout << "Every row should have an index length of: " << length << endl;
+  int a = 0;
+  
+
+  for (int j = rows; j > 0;  j--)
+  {
+    int x = pow(2,j-1) -1;	
+    if ( x == 0)
+      {
+	
+	for (int lastforloopipromise = 0; lastforloopipromise < z; lastforloopipromise++)
+	  {
+	    
+	    cout <<" ";
+	  }
+	int l = log10(intarray[0])+1;
+	if (l % 2 == 0)
+	  {
+	    cout << "0" ; // add a 0 if it has even nums of digits
+	  }
+	cout << intarray[0] << endl;
+        
+      }
+    else
+      {
+	for (int i = 0; i<= length;)
+	  {
+         
+	    if (i == elementsindex[a])
+	      {
+		if (intarray[x] == -2)
+		  {
+		    for( int k = 0; k <= longestnumber; k++ )
+		      {
+			cout << " ";
+		      }
+		    i+= longestnumber;
+		    a++;
+		    x++;
+		  }
+		else if ((intarray[x] != -1) && (x !=0))
+		  {
+		    int p = log10(intarray[x])+1;
+		    if (p <= longestnumber)
+		      {
+			for (int b = 0; b< longestnumber-p; b++)
+			  {
+			    cout << "0"; // 1 -> 001 if the longest digit is 3
+			  }
+			cout << intarray[x];
+			i += longestnumber;
+		  
+			a++;
+			x++;
+		      }
+		  }
+		else
+		  {
+		    break;
+		  }
+	 
+	      }
+	    else
+	      {
+		cout <<" ";
+		i++;
+	      }
+	  }
+      }
+    cout << endl;
+  
+  }
+  
+ 
+}
+
 
